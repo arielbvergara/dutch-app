7b38c12590d6b39752e0cf0afe3843ee0a5af07e
ariel.vergara <ariel.vergara@dustin.com>
2024-02-26 20:18:18 +0100

    Adding basic logic to fetch data

---
+++ src/app/api/openai/route.js
@@ -0,0 +1,38 @@
+import OpenAI from 'openai';
+import { NextResponse } from 'next/server';
+
+
+export async function POST(request) {
+
+    const body = await request.json();
+    
+    const openai = new OpenAI({
+      apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+    });
+    
+    try {
+      const result = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            "role": "system",
+            "content": "You are a system that translate and conjugate a verb from english or spanish to Dutch and conjugate the verb in present perfect. You return the result in a json format like the following:\n\n{\n    \"infinitive\"\n    \"present_perfect\": [{\n        \"pronoun\"\n        \"aux_verb\"\n        \"verb\"\n    }]\n}"
+          },
+          {
+            "role": "user",
+            "content": body.prompt
+          }
+        ],
+        temperature: 1,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      });
+
+      return NextResponse.json({message: result}, {status: 200})
+    } catch (error) {
+      return NextResponse.json({message: 'Error in API:' + error}, {status: 500})
+    }
+
+}
